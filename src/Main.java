6d6ac28522a5adaf5b226211b6f8d16bca359a07
psy-programmer <psy-programmer@outlook.com>
2020-07-03 22:06:32 +0300

    for studens

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//code by Zoia Klocheva
+
+import experimentClasses.Child;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.output();
+    }
+
+}
---
+++ src/experimentClasses/Child.java
@@ -0,0 +1,7 @@
+package experimentClasses;
+
+public class Child extends Parent {
+    public void output() {
+        System.out.println("num1 " + num1 + ", num2 " + getNum2() + ", num3 " + num3 + ", num4 " + num4);
+    }
+}
